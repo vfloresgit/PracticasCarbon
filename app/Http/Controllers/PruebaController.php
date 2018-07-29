1d7739fefc84760e7e42fdd1987c50be6e32724e
victor hugo <victorfq18@gmail.com>
2018-07-29 00:48:19 -0500

    prueba

---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PruebaController extends Controller
+{
+    //
+    public function hola(){
+    	echo "hola";
+    }
+}
