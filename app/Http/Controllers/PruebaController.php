259035b42e8847abaaedb517d0df259c5b1fb29f
victor hugo <victorfq18@gmail.com>
2018-07-29 00:49:48 -0500

    rama_andre prueba

---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PruebaController extends Controller
+{
+    //
+    public function hola(){
+    	echo "hola";
+    }
+    public function getAll(){
+    	
+    }
+}
