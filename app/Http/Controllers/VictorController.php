09a777f5516bc4b6a18e2d775edd62ff8f3eb293
victor hugo <victorfq18@gmail.com>
2018-07-28 22:47:42 -0500

    envio con victorcontroller

---
+++ app/Http/Controllers/VictorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VictorController extends Controller
+{
+    //
+    public function creacion(){
+    	echo "hola creacion";
+    }
+}
