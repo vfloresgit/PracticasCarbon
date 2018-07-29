a43a9b54e3db9a553dced91e44ec9fc352b12d66
victor hugo <victorfq18@gmail.com>
2018-07-29 00:45:35 -0500

    push

---
+++ app/Http/Controllers/FusionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FusionController extends Controller
+{
+    //
+    public function pruebafusion(){
+    	echo "fusion";
+    }
+    public function holabrother(){
+
+    }
+    public function holabrother2(){
+    	
+    }
+    public function push(){
+    	echo "hola push";
+    }
+}
