af4793fd52d14cf076d6f2264c843640d8a51431
victor hugo <victorfq18@gmail.com>
2018-07-28 23:08:46 -0500

    envio con mergecontroller

---
+++ app/Http/Controllers/RamaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RamaController extends Controller
+{
+    //ç
+    public function holarama(){
+    	echo "hola rama";
+    }
+}
